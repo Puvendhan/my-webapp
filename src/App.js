dfa6204ee5010abb3206df74f0253925349a976e
Puvendhan <puvendhan@Puvendhans-MacBook-Pro.local>
2023-08-08 00:16:41 +0530

    added the files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Welcome to Declop</h2>
+      <video width="700px" height="400px" controls>
+        <source src="https://d1ohz1rdqtg64n.cloudfront.net/sample-10s.mp4" type="video/mp4" />
+      </video>
+    </div>
+  );
+}
+
+export default App;
